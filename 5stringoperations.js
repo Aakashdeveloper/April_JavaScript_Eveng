4e3e44fcbf16c31eda2757a9e6d3f897a73624ec
aakashdeveloper <ahanda205@gmail.com>
2021-04-02 21:13:08 +0530

    storage

--- 5stringoperations.js
+++ 5stringoperations.js
@@ -51,3 +51,83 @@ city.slice(1).toLowerCase()
 "msterdam"
 city.charAt(0).toUpperCase()+city.slice(1).toLowerCase()
 "Amsterdam"
+
+var city = "Amsterdam"
+undefined
+city.slice(1)
+"msterdam"
+city.slice(2)
+"sterdam"
+city.slice(2,5)
+"ste"
+city.slice(1,5)
+"mste"
+city.slice(0,5)
+"Amste"
+city.slice(0,11)
+"Amsterdam"
+city.slice(-1)
+"m"
+city.slice(-2)
+"am"
+city.slice(-3)
+"dam"
+city.slice(3,-3)
+"ter"
+city.slice(1,-4)
+"mste"
+city.slice(-4,1)
+""
+city.slice(-4,-1)
+"rda"
+city.slice(-1,-4)
+""
+
+var a = "i am learning javascript"
+a.replace('javascript','js')
+"i am learning js"
+
+var a = "javascript i am learning javascript"
+a.replace('javascript','js')
+"js i am learning javascript"
+a.replace(/javascript/g,'js')
+"js i am learning js"
+
+var name = " Bhumika . "
+undefined
+name.replace(/ /g,'')
+"Bhumika."
+name.replace(/ /g,'-')
+"-Bhumika-.-"
+name
+" Bhumika . "
+
+
+var url = "https://github.com/Aakashdeveloper/Angular5-Seed"
+
+var url = "https://github.com/Aakashdeveloper/Angular5-Seed"
+undefined
+url.split('/')
+(5) ["https:", "", "github.com", "Aakashdeveloper", "Angular5-Seed"]0: "https:"1: ""2: "github.com"3: "Aakashdeveloper"4: "Angular5-Seed"length: 5__proto__: Array(0)
+var a = "javascript i am learning javascript"
+
+undefined
+a.split(' ')
+(5) ["javascript", "i", "am", "learning", "javascript"]
+
+var a = "javascript"
+a.split('')
+(10) ["j", "a", "v", "a", "s", "c", "r", "i", "p", "t"]
+
+var a = ["j", "a", "v", "a", "s", "c", "r", "i", "p", "t"]
+
+a.toString()
+"j,a,v,a,s,c,r,i,p,t"
+a.toString().replace(/,/g,'')
+"javascript"
+
+var a = 9879868
+a.toString().split()
+["9879868"]
+a.toString().split('')
+(7) ["9", "8", "7", "9", "8", "6", "8"]
---
+++ 8storage.js
@@ -0,0 +1,12 @@
+localStorage
+> persistant Storage
+> wrt to browser
+> we have to remove manully
+
+sessionStorage
+> temp Storage
+> save wrt to tab
+
+cookies
+> save wrt to website
+> expire in time
